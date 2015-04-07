9850511ca9b81e0f61514a5ae656580a7f3cba9a
Chintan Shah <chintan@wohlig.com>
2015-04-08 04:33:53 +0530

    check htis

--- application/models/json_model.php
+++ application/models/json_model.php
@@ -33,7 +33,17 @@ class Json_model extends CI_Model {
 INNER  JOIN `predicto_predictionteam` as `team1` ON `predicto_prediction`.`id`=`team1`.`prediction`  AND `team1`.`order`=1
 INNER  JOIN `predicto_predictionteam` as `team2` ON `predicto_prediction`.`id`=`team2`.`prediction` AND `team2`.`order`=2
 INNER  JOIN `predicto_teamgroup` as `team11` ON `team1`.`teamgroup`=`team11`.`id`
-INNER  JOIN `predicto_teamgroup` as `team22` ON `team2`.`teamgroup`=`team22`.`id`")->result();
+INNER  JOIN `predicto_teamgroup` as `team22` ON `team2`.`teamgroup`=`team22`.`id`
+
+")->result();
+        foreach ($prediction as $predict) {
+            $predictioncount = $this->db->query("SELECT COUNT(`id`) as `count` FROM `predicto_userprediction` WHERE `prediction`='$predict->id' GROUP BY `teamgroup`")->result();
+            if (($predictioncount[0]->count + $predictioncount[1]->count) > 0) {
+                $prediction->team1percent = $predictioncount[0]->count / ($predictioncount[0]->count + $predictioncount[1]->count) * 100;
+            } else {
+                $prediction->team1percent = - 1;
+            }
+        }
         return $prediction;
     }
     function userpredicts($team, $prediction) {
@@ -47,21 +57,33 @@ INNER  JOIN `predicto_teamgroup` as `team22` ON `team2`.`teamgroup`=`team22`.`id
         }
         return true;
     }
-    function getpredictionforuser($prediction) {
+    function getpredictionforuser($predict) {
         $prediction = $this->db->query("SELECT `predicto_prediction`.`id`,`predicto_prediction`.`name`,`predicto_prediction`.`status`,
 `predicto_prediction`.`predictionteam` as `winner`,`predicto_prediction`.`starttime`,`predicto_prediction`.`endtime`,
 `predicto_prediction`.`venue`,`team11`.`id` as `team1id`,`team22`.`id` as `team2id`,`team11`.`name` as `team1name`,`team22`.`name` as `team2name`
 FROM `predicto_prediction`
-INNER  JOIN `predicto_predictionteam` as `team1` ON `predicto_prediction`.`id`=`team1`.`prediction`  AND `team1`.`order`=1 AND `predicto_prediction`.`id`='$prediction'
+INNER  JOIN `predicto_predictionteam` as `team1` ON `predicto_prediction`.`id`=`team1`.`prediction`  AND `team1`.`order`=1 AND `predicto_prediction`.`id`='$predict'
 INNER  JOIN `predicto_predictionteam` as `team2` ON `predicto_prediction`.`id`=`team2`.`prediction` AND `team2`.`order`=2
 INNER  JOIN `predicto_teamgroup` as `team11` ON `team1`.`teamgroup`=`team11`.`id`
 INNER  JOIN `predicto_teamgroup` as `team22` ON `team2`.`teamgroup`=`team22`.`id` ")->row();
+        $predictioncount = $this->db->query("SELECT COUNT(`id`) as `count` FROM `predicto_userprediction` WHERE `prediction`='$prediction->id' GROUP BY `teamgroup`")->result();
+        if (($predictioncount[0]->count + $predictioncount[1]->count) > 0) {
+            $prediction->team1percent = $predictioncount[0]->count / ($predictioncount[0]->count + $predictioncount[1]->count) * 100;
+        } else {
+            $prediction->team1percent = - 1;
+        }
         $userid = $this->session->userdata("id");
         $query = $this->db->query("SELECT * FROM `predicto_userprediction` WHERE `user`='$userid' AND `teamgroup`='$team'");
         $querynum = $query->num_rows();
         if ($querynum != 0) {
             $query = $query->row();
             $prediction->predicted = $query->prediction;
+        }
+        $query2 = $this->db->query("SELECT * FROM `predicto_predictionhash` WHERE `prediction`='$predict'");
+        $querynum2 = $query2->num_rows();
+        $hash = 0;
+        if ($querynum2 != 0) {
+            $hash = $query2->result();
         }
         return $prediction;
     }
