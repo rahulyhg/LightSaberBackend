3109bb7f33399c2a7d4ab92712a1355337f59b5a
Chintan Shah <chintan@wohlig.com>
2015-04-08 01:36:33 +0530

    change

--- application/models/json_model.php
+++ application/models/json_model.php
@@ -3,12 +3,10 @@ if (!defined('BASEPATH')) exit('No direct script access allowed');
 class Json_model extends CI_Model {
     function authenticate() {
         $is_logged_in = $this->session->userdata('logged_in');
-        //        print_r($is_logged_in);
         if ($is_logged_in != 'true' || !isset($is_logged_in)) {
             return false;
         } //$is_logged_in !== 'true' || !isset( $is_logged_in )
         else {
-            $userid = $this->session->userdata('id');
             return $this->session->all_userdata();
         }
     }
@@ -17,9 +15,15 @@ class Json_model extends CI_Model {
     function getuserdetails() {
         $userid=$this->session->userdata("id");
         $user=$this->db->query("SELECT * FROM `user` WHERE `id`='$userid'")->row();
-        $user->prediction=$this->db->query("SELECT SUM(*) as `prediction` FROM `predicto_userprediction` WHERE `user`='$userid'")->row();
+        $user->prediction=$this->db->query("SELECT COUNT(`id`) as `prediction` FROM `predicto_userprediction` WHERE `user`='$userid'")->row();
         $user->prediction=$user->prediction->prediction;
         return $user;
+    }
+
+    function getpredictions() {
+        $userid=$this->session->userdata("id");
+        $user=$this->db->query("SELECT * FROM `user` WHERE `id`='$userid'")->row();
+      
     }
 
 
