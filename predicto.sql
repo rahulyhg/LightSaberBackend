25bcb3f700f90d554d0c636ae774c5453cbf1f33
Abhay Amin <pixoloproductions@gmail.com>
2015-04-08 06:53:51 +0530

    SQL file

--- predicto.sql
+++ predicto.sql
@@ -3,7 +3,7 @@
 -- http://www.phpmyadmin.net
 --
 -- Host: 127.0.0.1
--- Generation Time: Apr 07, 2015 at 11:05 PM
+-- Generation Time: Apr 08, 2015 at 03:14 AM
 -- Server version: 5.6.20
 -- PHP Version: 5.5.15
 
@@ -145,19 +145,73 @@ CREATE TABLE IF NOT EXISTS `predicto_prediction` (
   `order` int(11) NOT NULL,
   `starttime` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
   `venue` varchar(255) NOT NULL
-) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=8 ;
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=61 ;
 
 --
 -- Dumping data for table `predicto_prediction`
 --
 
 INSERT INTO `predicto_prediction` (`id`, `predictiongroup`, `name`, `status`, `predictionteam`, `endtime`, `order`, `starttime`, `venue`) VALUES
-(1, 1, 'Match 1', 1, 0, '2015-04-08 20:00:15', 1, '2015-04-07 20:42:15', 'Eden Gardens, Kolkata'),
-(2, 1, 'Match 2', 1, 0, '2015-04-09 20:00:15', 2, '2015-04-07 20:42:15', 'M. A. Chidambaram Stadium, Chennai'),
-(3, 1, 'Match 3', 1, 0, '2015-04-10 20:00:15', 3, '2015-04-07 20:42:15', 'MCA International Stadium, Pune'),
-(4, 1, 'Match 3', 1, 0, '2015-04-11 16:00:15', 3, '2015-04-07 20:42:15', 'M. A. Chidambaram Stadium, Chennai'),
-(5, 1, 'Match 4', 1, 0, '2015-04-11 20:00:15', 4, '2015-04-07 20:42:15', 'Eden Gardens, Kolkata'),
-(6, 1, 'Match 5', 1, 0, '', 0, '2015-04-07 20:57:54', 'Eden Gardens, Kolkata');
+(1, 1, 'Match 1', 1, 0, '08-04-15 20:00', 1, '2008-04-15 14:30:00', 'Eden Gardens, Kolkata'),
+(2, 1, 'Match 2', 1, 0, '09-04-15 20:00', 1, '2009-04-15 14:30:00', 'M. A. Chidambaram Stadium, Chennai'),
+(3, 1, 'Match 3', 1, 0, '10-04-15 20:00', 1, '2010-04-15 14:30:00', 'MCA International Stadium, Pune'),
+(4, 1, 'Match 4', 1, 0, '11-04-15 20:00', 1, '2011-04-15 14:30:00', 'M. A. Chidambaram Stadium, Chennai'),
+(5, 1, 'Match 5', 0, 0, '12-04-15 20:00', 1, '2012-04-15 14:30:00', 'Eden Gardens, Kolkata'),
+(6, 1, 'Match 6', 0, 0, '13-04-15 20:00', 1, '2013-04-15 14:30:00', 'Ferozeshah Kotla, Delhi'),
+(7, 1, 'Match 7', 0, 0, '14-04-15 20:00', 1, '2014-04-15 14:30:00', 'Wankhede Stadium, Mumbai'),
+(8, 1, 'Match 8', 0, 0, '15-04-15 20:00', 1, '2015-04-15 14:30:00', 'M. Chinnaswamy Stadium, Bengaluru'),
+(9, 1, 'Match 9', 0, 0, '16-04-15 20:00', 1, '2016-04-15 14:30:00', 'Sardar Patel Stadium, Ahmedabad'),
+(10, 1, 'Match 10', 0, 0, '17-04-15 20:00', 1, '2017-04-15 14:30:00', 'MCA International Stadium, Pune'),
+(11, 1, 'Match 11', 0, 0, '18-04-15 20:00', 1, '2018-04-15 14:30:00', 'ACA-VDCA Stadium, Visakhapatnam'),
+(12, 1, 'Match 12', 0, 0, '19-04-15 20:00', 1, '2019-04-15 14:30:00', 'Wankhede Stadium, Mumbai'),
+(13, 1, 'Match 13', 0, 0, '20-04-15 20:00', 1, '2020-04-15 14:30:00', 'ACA-VDCA Stadium, Visakhapatnam'),
+(14, 1, 'Match 14', 0, 0, '21-04-15 20:00', 1, '2021-04-15 14:30:00', 'MCA International Stadium, Pune'),
+(15, 1, 'Match 15', 0, 0, '22-04-15 20:00', 1, '2022-04-15 14:30:00', 'Sardar Patel Stadium, Ahmedabad'),
+(16, 1, 'Match 16', 0, 0, '23-04-15 20:00', 1, '2023-04-15 14:30:00', 'M. Chinnaswamy Stadium, Bengaluru'),
+(17, 1, 'Match 17', 0, 0, '24-04-15 20:00', 1, '2024-04-15 14:30:00', 'Ferozeshah Kotla, Delhi'),
+(18, 1, 'Match 18', 0, 0, '25-04-15 20:00', 1, '2025-04-15 14:30:00', 'Sardar Patel Stadium, Ahmedabad'),
+(19, 1, 'Match 19', 0, 0, '26-04-15 20:00', 1, '2026-04-15 14:30:00', 'ACA-VDCA Stadium, Visakhapatnam'),
+(20, 1, 'Match 20', 0, 0, '27-04-15 20:00', 1, '2027-04-15 14:30:00', 'M. Chinnaswamy Stadium, Bengaluru'),
+(21, 1, 'Match 21', 0, 0, '28-04-15 20:00', 1, '2028-04-15 14:30:00', 'Ferozeshah Kotla, Delhi'),
+(22, 1, 'Match 22', 0, 0, '29-04-15 20:00', 1, '2029-04-15 14:30:00', 'Sardar Patel Stadium, Ahmedabad'),
+(23, 1, 'Match 23', 0, 0, '30-04-15 20:00', 1, '2030-04-15 14:30:00', 'Wankhede Stadium, Mumbai'),
+(24, 1, 'Match 24', 0, 0, '01-05-15 20:00', 1, '2001-05-15 14:30:00', 'M. A. Chidambaram Stadium, Chennai'),
+(25, 1, 'Match 25', 0, 0, '02-05-15 20:00', 1, '2002-05-15 14:30:00', 'Eden Gardens, Kolkata'),
+(26, 1, 'Match 26', 0, 0, '03-05-15 20:00', 1, '2003-05-15 14:30:00', 'Ferozeshah Kotla, Delhi'),
+(27, 1, 'Match 27', 0, 0, '04-05-15 20:00', 1, '2004-05-15 14:30:00', 'Punjab Cricket Association Stadium, Mohali'),
+(28, 1, 'Match 28', 0, 0, '05-05-15 20:00', 1, '2005-05-15 14:30:00', 'M. A. Chidambaram Stadium, Chennai'),
+(29, 1, 'Match 29', 0, 0, '06-05-15 20:00', 1, '2006-05-15 14:30:00', 'M. Chinnaswamy Stadium, Bengaluru'),
+(30, 1, 'Match 30', 0, 0, '07-05-15 20:00', 1, '2007-05-15 14:30:00', 'Eden Gardens, Kolkata'),
+(31, 1, 'Match 31', 0, 0, '08-05-15 20:00', 1, '2008-05-15 14:30:00', 'Ferozeshah Kotla, Delhi'),
+(32, 1, 'Match 32', 0, 0, '09-05-15 20:00', 1, '2009-05-15 14:30:00', 'Wankhede Stadium, Mumbai'),
+(33, 1, 'Match 33', 0, 0, '10-05-15 20:00', 1, '2010-05-15 14:30:00', 'M. Chinnaswamy Stadium, Bengaluru'),
+(34, 1, 'Match 34', 0, 0, '11-05-15 20:00', 1, '2011-05-15 14:30:00', 'Rajiv Gandhi International Cricket Stadium, Hyderabad'),
+(35, 1, 'Match 35', 0, 0, '12-05-15 20:00', 1, '2012-05-15 14:30:00', 'Punjab Cricket Association Stadium, Mohali'),
+(36, 1, 'Match 36', 0, 0, '13-05-15 20:00', 1, '2013-05-15 14:30:00', 'Brabourne Stadium, Mumbai'),
+(37, 1, 'Match 37', 0, 0, '14-05-15 20:00', 1, '2014-05-15 14:30:00', 'M. A. Chidambaram Stadium, Chennai'),
+(38, 1, 'Match 38', 0, 0, '15-05-15 20:00', 1, '2015-05-15 14:30:00', 'Eden Gardens, Kolkata'),
+(39, 1, 'Match 39', 0, 0, '16-05-15 20:00', 1, '2016-05-15 14:30:00', 'Wankhede Stadium, Mumbai'),
+(40, 1, 'Match 40', 0, 0, '17-05-15 20:00', 1, '2017-05-15 14:30:00', 'M. Chinnaswamy Stadium, Bengaluru'),
+(41, 1, 'Match 41', 0, 0, '18-05-15 20:00', 1, '2018-05-15 14:30:00', 'Brabourne Stadium, Mumbai'),
+(42, 1, 'Match 42', 0, 0, '19-05-15 20:00', 1, '2019-05-15 14:30:00', 'Eden Gardens, Kolkata'),
+(43, 1, 'Match 43', 0, 0, '20-05-15 20:00', 1, '2020-05-15 14:30:00', 'M. A. Chidambaram Stadium, Chennai'),
+(44, 1, 'Match 44', 0, 0, '21-05-15 20:00', 1, '2021-05-15 14:30:00', 'Eden Gardens, Kolkata'),
+(45, 1, 'Match 45', 0, 0, '22-05-15 20:00', 1, '2022-05-15 14:30:00', 'Chhattisgarh International Cricket Stadium, Raipur'),
+(46, 1, 'Match 46', 0, 0, '23-05-15 20:00', 1, '2023-05-15 14:30:00', 'Wankhede Stadium, Mumbai'),
+(47, 1, 'Match 47', 0, 0, '24-05-15 20:00', 1, '2024-05-15 14:30:00', 'M. A. Chidambaram Stadium, Chennai'),
+(48, 1, 'Match 48', 0, 0, '25-05-15 20:00', 1, '2025-05-15 14:30:00', 'Rajiv Gandhi International Cricket Stadium, Hyderabad'),
+(49, 1, 'Match 49', 0, 0, '26-05-15 20:00', 1, '2026-05-15 14:30:00', 'Chhattisgarh International Cricket Stadium, Raipur'),
+(50, 1, 'Match 50', 0, 0, '27-05-15 20:00', 1, '2027-05-15 14:30:00', 'Punjab Cricket Association Stadium, Mohali'),
+(51, 1, 'Match 51', 0, 0, '28-05-15 20:00', 1, '2028-05-15 14:30:00', 'Wankhede Stadium, Mumbai'),
+(52, 1, 'Match 52', 0, 0, '29-05-15 20:00', 1, '2029-05-15 14:30:00', 'Rajiv Gandhi International Cricket Stadium, Hyderabad'),
+(53, 1, 'Match 53', 0, 0, '30-05-15 20:00', 1, '2030-05-15 14:30:00', 'Punjab Cricket Association Stadium, Mohali'),
+(54, 1, 'Match 54', 0, 0, '31-05-15 20:00', 1, '2031-05-15 14:30:00', 'Brabourne Stadium, Mumbai'),
+(55, 1, 'Match 55', 0, 0, '01-06-15 20:00', 1, '2001-06-15 14:30:00', 'M. Chinnaswamy Stadium, Bengaluru'),
+(56, 1, 'Match 56', 0, 0, '02-06-15 20:00', 1, '2002-06-15 14:30:00', 'Rajiv Gandhi International Cricket Stadium, Hyderabad'),
+(57, 1, 'Qualifier 1', 0, 0, '03-06-15 20:00', 1, '2003-06-15 14:30:00', 'TBD'),
+(58, 1, 'Eliminator', 0, 0, '04-06-15 20:00', 1, '2004-06-15 14:30:00', 'TBD'),
+(59, 1, 'Qualifier 2', 0, 0, '05-06-15 20:00', 1, '2005-06-15 14:30:00', 'TBD'),
+(60, 1, 'Final', 0, 0, '06-06-15 20:00', 1, '2006-06-15 14:30:00', 'Eden Gardens, Kolkata');
 
 -- --------------------------------------------------------
 
@@ -190,7 +244,15 @@ CREATE TABLE IF NOT EXISTS `predicto_predictionhash` (
 `id` int(11) NOT NULL,
   `prediction` int(11) NOT NULL,
   `hashtag` varchar(255) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 AUTO_INCREMENT=1 ;
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=3 ;
+
+--
+-- Dumping data for table `predicto_predictionhash`
+--
+
+INSERT INTO `predicto_predictionhash` (`id`, `prediction`, `hashtag`) VALUES
+(1, 1, '#ipl'),
+(2, 1, '#aap');
 
 -- --------------------------------------------------------
 
@@ -293,7 +355,16 @@ CREATE TABLE IF NOT EXISTS `predicto_userprediction` (
   `user` int(11) NOT NULL,
   `teamgroup` int(11) NOT NULL,
   `prediction` int(11) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 AUTO_INCREMENT=1 ;
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=11 ;
+
+--
+-- Dumping data for table `predicto_userprediction`
+--
+
+INSERT INTO `predicto_userprediction` (`id`, `user`, `teamgroup`, `prediction`) VALUES
+(7, 15, 2, 1),
+(9, 15, 2, 1),
+(10, 15, 0, 2);
 
 -- --------------------------------------------------------
 
@@ -362,22 +433,23 @@ CREATE TABLE IF NOT EXISTS `user` (
   `socialid` varchar(255) NOT NULL,
   `logintype` int(11) NOT NULL,
   `json` text NOT NULL,
-  `points` varchar(255) NOT NULL
-) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=15 ;
+  `points` double NOT NULL DEFAULT '0'
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=16 ;
 
 --
 -- Dumping data for table `user`
 --
 
 INSERT INTO `user` (`id`, `name`, `password`, `email`, `accesslevel`, `timestamp`, `status`, `image`, `username`, `socialid`, `logintype`, `json`, `points`) VALUES
-(1, 'wohlig', 'a63526467438df9566c508027d9cb06b', 'wohlig@wohlig.com', 1, '0000-00-00 00:00:00', 1, NULL, '', '', 0, '', ''),
-(4, 'pratik', '0cb2b62754dfd12b6ed0161d4b447df7', 'pratik@wohlig.com', 1, '2014-05-12 06:52:44', 1, NULL, 'pratik', '1', 1, '', ''),
-(5, 'wohlig123', 'wohlig123', 'wohlig1@wohlig.com', 1, '2014-05-12 06:52:44', 1, NULL, '', '', 0, '', ''),
-(6, 'wohlig1', 'a63526467438df9566c508027d9cb06b', 'wohlig2@wohlig.com', 1, '2014-05-12 06:52:44', 1, NULL, '', '', 0, '', ''),
-(7, 'Avinash', '7b0a80efe0d324e937bbfc7716fb15d3', 'avinash@wohlig.com', 1, '2014-10-17 06:22:29', 1, NULL, '', '', 0, '', ''),
-(9, 'avinash', 'a208e5837519309129fa466b0c68396b', 'a@email.com', 2, '2014-12-03 11:06:19', 3, '', '', '123', 1, 'demojson', ''),
-(13, 'aaa', 'a208e5837519309129fa466b0c68396b', 'aaa3@email.com', 3, '2014-12-04 06:55:42', 3, NULL, '', '1', 2, 'userjson', ''),
-(14, 'chintanhappines', '', '', 2, '2015-04-07 19:12:05', 1, 'http://pbs.twimg.com/profile_images/465790248304115712/s0WXS5Si_normal.png', 'Chintan Shah', '121427044', 2, '', '');
+(1, 'wohlig', 'a63526467438df9566c508027d9cb06b', 'wohlig@wohlig.com', 1, '0000-00-00 00:00:00', 1, NULL, '', '', 0, '', 0),
+(4, 'pratik', '0cb2b62754dfd12b6ed0161d4b447df7', 'pratik@wohlig.com', 1, '2014-05-12 06:52:44', 1, NULL, 'pratik', '1', 1, '', 0),
+(5, 'wohlig123', 'wohlig123', 'wohlig1@wohlig.com', 1, '2014-05-12 06:52:44', 1, NULL, '', '', 0, '', 0),
+(6, 'wohlig1', 'a63526467438df9566c508027d9cb06b', 'wohlig2@wohlig.com', 1, '2014-05-12 06:52:44', 1, NULL, '', '', 0, '', 0),
+(7, 'Avinash', '7b0a80efe0d324e937bbfc7716fb15d3', 'avinash@wohlig.com', 1, '2014-10-17 06:22:29', 1, NULL, '', '', 0, '', 0),
+(9, 'avinash', 'a208e5837519309129fa466b0c68396b', 'a@email.com', 2, '2014-12-03 11:06:19', 3, '', '', '123', 1, 'demojson', 0),
+(13, 'aaa', 'a208e5837519309129fa466b0c68396b', 'aaa3@email.com', 3, '2014-12-04 06:55:42', 3, NULL, '', '1', 2, 'userjson', 0),
+(14, 'chintanhappines', '', '', 2, '2015-04-07 19:12:05', 1, 'http://pbs.twimg.com/profile_images/465790248304115712/s0WXS5Si_normal.png', 'Chintan Shah', '121427044', 2, '', 0),
+(15, 'aminAbhay', '', '', 2, '2015-04-07 22:01:11', 1, 'http://pbs.twimg.com/profile_images/585536031530872832/TSb0uQjV_normal.jpg', 'Abhay Amin', '1709476142', 2, '', 0);
 
 -- --------------------------------------------------------
 
@@ -554,7 +626,7 @@ MODIFY `id` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=15;
 -- AUTO_INCREMENT for table `predicto_prediction`
 --
 ALTER TABLE `predicto_prediction`
-MODIFY `id` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=8;
+MODIFY `id` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=61;
 --
 -- AUTO_INCREMENT for table `predicto_predictiongroup`
 --
@@ -564,7 +636,7 @@ MODIFY `id` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=2;
 -- AUTO_INCREMENT for table `predicto_predictionhash`
 --
 ALTER TABLE `predicto_predictionhash`
-MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+MODIFY `id` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=3;
 --
 -- AUTO_INCREMENT for table `predicto_predictionteam`
 --
@@ -589,7 +661,7 @@ MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
 -- AUTO_INCREMENT for table `predicto_userprediction`
 --
 ALTER TABLE `predicto_userprediction`
-MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+MODIFY `id` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=11;
 --
 -- AUTO_INCREMENT for table `predicto_usershare`
 --
@@ -609,7 +681,7 @@ MODIFY `id` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=6;
 -- AUTO_INCREMENT for table `user`
 --
 ALTER TABLE `user`
-MODIFY `id` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=15;
+MODIFY `id` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=16;
 --
 -- AUTO_INCREMENT for table `userlog`
 --
