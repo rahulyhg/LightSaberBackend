8e2d3e4f54d83dadd915067e29c4e98601f4ddfe
Chintan Shah <chintan@wohlig.com>
2015-04-08 20:53:24 +0530

    for timestamp

--- application/models/json_model.php
+++ application/models/json_model.php
@@ -26,7 +26,7 @@ class Json_model extends CI_Model {
     function getpredictions() {
         $userid = $this->session->userdata("id");
         $prediction = $this->db->query("SELECT `predicto_prediction`.`id`,`predicto_prediction`.`name`,`predicto_prediction`.`status`,
-`predicto_prediction`.`predictionteam` as `winner`,`predicto_prediction`.`starttime`,`predicto_prediction`.`endtime`,
+`predicto_prediction`.`predictionteam` as `winner`,UNIX_TIMESTAMP(`predicto_prediction`.`starttime`)*1000 as `starttime`,UNIX_TIMESTAMP(`predicto_prediction`.`endtime`)*1000 as `endtime`,
 `predicto_prediction`.`venue`,`team11`.`id` as `team1id`,`team22`.`id` as `team2id`,`team11`.`name` as `team1name`,`team22`.`name` as `team2name`
  FROM `predicto_prediction`
 
@@ -67,7 +67,7 @@ INNER  JOIN `predicto_teamgroup` as `team22` ON `team2`.`teamgroup`=`team22`.`id
     function getpredictionforuser($predict) {
         $userid = $this->session->userdata("id");
         $prediction = $this->db->query("SELECT `predicto_prediction`.`id`,`predicto_prediction`.`name`,`predicto_prediction`.`status`,
-`predicto_prediction`.`predictionteam` as `winner`,`predicto_prediction`.`starttime`,`predicto_prediction`.`endtime`,
+`predicto_prediction`.`predictionteam` as `winner`,FLOOR(UNIX_TIMESTAMP(`predicto_prediction`.`starttime`)*1000) as `starttime`,FLOOR(UNIX_TIMESTAMP(`predicto_prediction`.`endtime`)*1000) as `endtime`,
 `predicto_prediction`.`venue`,`team11`.`id` as `team1id`,`team22`.`id` as `team2id`,`team11`.`name` as `team1name`,`team22`.`name` as `team2name`
 FROM `predicto_prediction`
 INNER  JOIN `predicto_predictionteam` as `team1` ON `predicto_prediction`.`id`=`team1`.`prediction`  AND `team1`.`order`=1 AND `predicto_prediction`.`id`='$predict'
