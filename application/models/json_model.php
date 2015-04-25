b3397510f611fe30d4c34a9b154bfdf934f7d181
Chintan Shah <chintan@wohlig.com>
2015-04-25 10:22:37 +0530

    chnage it

--- application/models/json_model.php
+++ application/models/json_model.php
@@ -76,7 +76,7 @@ INNER  JOIN `predicto_teamgroup` as `team22` ON `team2`.`teamgroup`=`team22`.`id
     }
     function userpredicts($userid,$team, $prediction) {
         $query = $this->db->query("SELECT * FROM `predicto_userprediction` WHERE `user`='$userid' AND `prediction`='$prediction'");
-		$query1=$this->db->query("SELECT * FROM `predicto_prediction` WHERE `prediction` = '$prediction'")->row();
+		$query1=$this->db->query("SELECT * FROM `predicto_prediction` WHERE `id` = '$prediction'")->row();
 		if($query1->status=='0')
 		{
 			return false;
