0b59fdd1b3f5cebe4d670080332953fa7b5c3c12
Chintan Shah <chintan@wohlig.com>
2015-05-09 13:09:40 +0530

    changes in session

--- application/config/config.php
+++ application/config/config.php
@@ -265,15 +265,15 @@ $config['encryption_key'] = 'chintan';
 | 'sess_time_to_update'		= how many seconds between CI refreshing Session Information
 |
 */
-$config['sess_cookie_name']		= 'ci_session';
+$config['sess_cookie_name']		= 'predicto_session';
 $config['sess_expiration']		= 0;
 $config['sess_expire_on_close']	= FALSE;
-$config['sess_encrypt_cookie']	= FALSE;
+$config['sess_encrypt_cookie']	= TRUE;
 $config['sess_use_database']	= TRUE;
 $config['sess_table_name']		= 'ci_sessions';
-$config['sess_match_ip']		= FALSE;
+$config['sess_match_ip']		= TRUE;
 $config['sess_match_useragent']	= TRUE;
-$config['sess_time_to_update']	= 3000;
+$config['sess_time_to_update']	= 1000000;
 
 /*
 |--------------------------------------------------------------------------
