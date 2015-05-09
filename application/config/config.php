f7471cb5b9bcff6b16debd99e9b7cd3b4bec770f
Chintan Shah <chintan@wohlig.com>
2015-05-09 13:12:16 +0530

    yes abhi

--- application/config/config.php
+++ application/config/config.php
@@ -266,14 +266,14 @@ $config['encryption_key'] = 'chintan';
 |
 */
 $config['sess_cookie_name']		= 'predicto_session';
-$config['sess_expiration']		= 0;
+$config['sess_expiration']		= 31536864;
 $config['sess_expire_on_close']	= FALSE;
 $config['sess_encrypt_cookie']	= TRUE;
 $config['sess_use_database']	= TRUE;
 $config['sess_table_name']		= 'ci_sessions';
 $config['sess_match_ip']		= TRUE;
 $config['sess_match_useragent']	= TRUE;
-$config['sess_time_to_update']	= 31585600;
+$config['sess_time_to_update']	= 31536864;
 
 /*
 |--------------------------------------------------------------------------
